6bcd270d6af8eee249281f25305f79ada2274619
rebeckao <rebecka.oefverholm@gmail.com>
2019-04-09 19:09:07 +0200

    Fixed the first test!

--- src/test/java/DayOneCaptchaTest.java
+++ src/test/java/DayOneCaptchaTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,10 +14,16 @@ class DayOneCaptchaTest {
             "1234, 0",
             "91212129, 9"
     })
-    void captchaSumTest(String input, int expected){
+    void captchaSumTest(String input, int expected) {
         DayOneCaptcha dayOneCaptcha = new DayOneCaptcha();
-        assertEquals(expected,dayOneCaptcha.captchaSum(input));
+        assertEquals(expected, dayOneCaptcha.captchaSum(input));
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources="/day1.txt")
+    void captchaFileTest(String input) {
+        DayOneCaptcha dayOneCaptcha = new DayOneCaptcha();
+        assertEquals(1089, dayOneCaptcha.captchaSum(input));
+    }
 
 }
