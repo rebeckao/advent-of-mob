0014308d958da97eb7b40bc20762ba3d5a5ce735
rebeckao <rebecka.oefverholm@gmail.com>
2019-05-21 19:58:24 +0200

    Step 2, WIP fibonacci spiral memory

--- src/main/java/DayThreeSpiralMemory.java
+++ src/main/java/DayThreeSpiralMemory.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DayThreeSpiralMemory {
     int lastCornerInLayer(int layer) {
         return (int) Math.pow(layer * 2 + 1, 2);
@@ -9,7 +12,7 @@ public class DayThreeSpiralMemory {
         int lastCornerInLayer = lastCornerInLayer(layer);
         int distanceInLayer = value - lastCornerInPreviousLayer;
         int layerLength = lastCornerInLayer - lastCornerInPreviousLayer;
-        int distanceFromPreviousCorner = layerLength == 0? 0 : distanceInLayer % (layerLength/4);
+        int distanceFromPreviousCorner = layerLength == 0 ? 0 : distanceInLayer % (layerLength / 4);
         int offsetFromMiddle = Math.abs(distanceFromPreviousCorner - layer);
         return offsetFromMiddle + layer;
     }
@@ -21,4 +24,58 @@ public class DayThreeSpiralMemory {
         }
         return layer;
     }
+
+
+    Position getNextPosition(Position previous, Position current) {
+        if (current.x > 0 && current.y == -current.x) {
+            return Position.builder().x(current.x - 1).y(current.y).build();
+        }
+        if (current.x < 0 && current.y == current.x) {
+            return Position.builder().x(current.x).y(current.y + 1).build();
+        }
+        if (current.x < 0 && current.y == -current.x) {
+            return Position.builder().x(current.x + 1).y(current.y).build();
+        }
+        if (current.x > 0 && current.y == current.x - 1) {
+            return Position.builder().x(current.x).y(current.y - 1).build();
+        }
+
+        return Position.builder()
+                .x(current.x + (current.x - previous.x))
+                .y(current.y + (current.y - previous.y))
+                .build();
+    }
+
+    int calculateSpiralFibonacci(int pos) {
+        Position prev = Position.builder().x(1).y(0).build();
+        Position current = Position.builder().x(1).y(-1).build();
+        Map<Position, Integer> map = new HashMap<>();
+        map.put(Position.builder().x(0).y(0).build(), 1);
+        map.put(Position.builder().x(1).y(0).build(), 1);
+
+        int sumOfAll = 1;
+        for (int i = 2; i <= pos; i++) {
+            sumOfAll = 0;
+            for (int x = current.x - 1; x <= current.x + 1; x++) {
+                for (int y = current.y - 1; y <= current.y + 1; y++) {
+                    sumOfAll += map.getOrDefault(Position.builder().x(x).y(y).build(), 0);
+                }
+            }
+            map.put(current, sumOfAll);
+            Position next = getNextPosition(prev, current);
+            prev = current;
+            current = next;
+
+        }
+        return sumOfAll;
+    }
+
+    /*private void sumOfAdjacent(Position current, Map<Position, Integer> map) {
+        IntStream.range(current.x - 1, current.x + 2)
+                .mapToObj(x -> IntStream.range(current.y - 1, current.y + 2)
+                        .mapToObj(y -> Position.builder().x(x).y(y).build()))
+                .flatMap(Function.identity())
+                .mapToInt(p -> map.getOrDefault(p, 0))
+                .sum();
+    }*/
 }
--- src/test/java/DayThreeSpiralMemoryTest.java
+++ src/test/java/DayThreeSpiralMemoryTest.java
@@ -22,4 +22,21 @@ class DayThreeSpiralMemoryTest {
     void lastCornerInLayer(int value, int cornerValue) {
         assertEquals(cornerValue, new DayThreeSpiralMemory().lastCornerInLayer(value));
     }
+
+    @ParameterizedTest
+    @CsvSource({"1, 1", "2, 1", "3, 2", "4, 4", "5, 5", "6, 10", "7, 11", "8, 23", "9, 25",
+            "10, 26", "11, 54", "12, 57", "13, 59", "14, 122", "15, 133", "16, 142", "17, 147", "18, 304", "19, 330"})
+    void spiralFibonacci(int pos, int value) {
+        assertEquals(value, new DayThreeSpiralMemory().calculateSpiralFibonacci(pos));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0, 1,0, 1,-1", "1,0, 1,-1, 0,-1", "1,-1, 0,-1, -1,-1", "0,-1, -1,-1, -1,0", "-1,-1, -1,0, -1,1",
+    "-1,0, -1,1, 0,1", "-1,1, 0,1, 1,1", "0,1, 1,1, 2,1"})
+    void getNextPosition(int prevX, int prevY, int curX, int curY, int nextX, int nextY) {
+        Position previous = Position.builder().x(prevX).y(prevY).build();
+        Position current = Position.builder().x(curX).y(curY).build();
+        Position next = Position.builder().x(nextX).y(nextY).build();
+        assertEquals(next, new DayThreeSpiralMemory().getNextPosition(previous, current));
+    }
 }
