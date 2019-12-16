f8cb821f2bf580cfd1a273ff24363946f93a63f4
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-16 19:53:30 +0100

    Day 7 Part 2

--- src/main/java/Day07CircuitTowers.kt
+++ src/main/java/Day07CircuitTowers.kt
@@ -3,8 +3,24 @@ import java.util.*
 class Day07CircuitTowers {
     fun nameOfBottomTower(towers: List<String>): String {
         val towerTowers = towers.map { parseTower(it) }
+        return nameOfRootTower(towerTowers)
+    }
+
+    private fun nameOfRootTower(towerTowers: List<Tower>): String {
         val nonRootTowersNames: List<String> = towerTowers.flatMap { it.children }
         return towerTowers.map { it.name }.find { !nonRootTowersNames.contains(it) }!!
+    }
+
+    fun correctedWeightOfBalancedTowers(towers: List<String>): Int {
+        val towerTowers = towers.map { parseTower(it) }
+        val nameOfRootTower = nameOfRootTower(towerTowers)
+        val rootTower = towerTowers.find { it.name == nameOfRootTower }!!
+        try {
+            rootTower.getWeight(towerTowers)
+            throw IllegalStateException("Should never get here")
+        } catch (ex: WrongWeightedChildException) {
+            return ex.desiredWeight
+        }
     }
 
     private fun parseTower(towerString: String): Tower {
@@ -20,5 +36,25 @@ class Day07CircuitTowers {
         return Tower(name, weight, children)
     }
 
-    data class Tower(val name: String, val weight: Int, val children: List<String>)
+    data class Tower(val name: String, val weight: Int, val children: List<String>) {
+        fun getWeight(towers: List<Tower>): Int {
+            if (children.isEmpty()) {
+                return weight
+            }
+
+            val childTowers = children.map { child -> towers.find { it.name == child }!! }
+            val childWeights = childTowers.map { it.getWeight(towers) }
+            val groupByWeight = childTowers.groupBy { it.getWeight(towers) }
+            val wrongWeightedChild = groupByWeight.values.find { it.size == 1 }
+            if (wrongWeightedChild != null) {
+                val correctTotalWeight = groupByWeight.entries.find { it.value.size > 1 } !!.key
+                val diff = wrongWeightedChild[0].getWeight(towers) - correctTotalWeight
+                throw WrongWeightedChildException(wrongWeightedChild[0].weight - diff)
+            }
+
+            return weight + childWeights.sum()
+        }
+    }
+
+    class WrongWeightedChildException(val desiredWeight: Int, message: String = "Wrong child") : RuntimeException(message)
 }
--- src/test/java/Day07CircuitTowersTest.kt
+++ src/test/java/Day07CircuitTowersTest.kt
@@ -6,27 +6,40 @@ import kotlin.streams.toList
 
 internal class Day07CircuitTowersTest {
 
+    private val towers = listOf("pbga (66)",
+            "xhth (57)",
+            "ebii (61)",
+            "havc (66)",
+            "ktlj (57)",
+            "fwft (72) -> ktlj, cntj, xhth",
+            "qoyq (66)",
+            "padx (45) -> pbga, havc, qoyq",
+            "tknk (41) -> ugml, padx, fwft",
+            "jptl (61)",
+            "ugml (68) -> gyxo, ebii, jptl",
+            "gyxo (61)",
+            "cntj (57)")
+
     @Test
     fun nameOfBottomTower() {
-        val towers = listOf("pbga (66)",
-                "xhth (57)",
-                "ebii (61)",
-                "havc (66)",
-                "ktlj (57)",
-                "fwft (72) -> ktlj, cntj, xhth",
-                "qoyq (66)",
-                "padx (45) -> pbga, havc, qoyq",
-                "tknk (41) -> ugml, padx, fwft",
-                "jptl (61)",
-                "ugml (68) -> gyxo, ebii, jptl",
-                "gyxo (61)",
-                "cntj (57)")
         assertEquals("tknk", Day07CircuitTowers().nameOfBottomTower(towers))
     }
 
     @Test
     fun nameOfBottomTowerForReal() {
         val towers = Files.lines(Paths.get("./src/test/resources/day7.txt")).toList()
-        assertEquals("tknk", Day07CircuitTowers().nameOfBottomTower(towers))
+        assertEquals("dgoocsw", Day07CircuitTowers().nameOfBottomTower(towers))
+    }
+
+    @Test
+    fun correctedWeightOfBalancedTowers() {
+        val correctedWeight = 60
+        assertEquals(correctedWeight,Day07CircuitTowers().correctedWeightOfBalancedTowers(towers))
+    }
+
+    @Test
+    fun correctedWeightOfBalancedTowersReal() {
+        val towers = Files.lines(Paths.get("./src/test/resources/day7.txt")).toList()
+        assertEquals(1275, Day07CircuitTowers().correctedWeightOfBalancedTowers(towers))
     }
 }
