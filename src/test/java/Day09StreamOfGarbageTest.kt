de44c6a2b71de6f85c457641eb9e97abfc25de88
rebeckao <rebecka.oefverholm@gmail.com>
2020-06-09 18:43:39 +0200

    problem solved

--- src/test/java/Day09StreamOfGarbageTest.kt
+++ src/test/java/Day09StreamOfGarbageTest.kt
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.nio.file.Files
+import java.nio.file.Paths
 
 internal class Day09StreamOfGarbageTest {
 
@@ -20,4 +23,11 @@ internal class Day09StreamOfGarbageTest {
     fun sumOfGroupScores(stream: String, expected: Int) {
         assertEquals(expected, Day09StreamOfGarbage(0, stream).sumOfGroupScores())
     }
+
+    @Test
+    fun sumOfGroupsScoresReal() {
+        val chars = Files.lines(Paths.get("./src/test/resources/day9.txt")).findFirst().orElse("0")
+        assertEquals(20530, Day09StreamOfGarbage(0, chars).sumOfGroupScores())
+    }
+
 }
