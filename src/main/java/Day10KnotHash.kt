878a660c1fed839724b4d862ef65d13f17952c73
FredF78 <fredrikf78@gmail.com>
2020-10-26 19:20:45 +0100

    gradle upgraded to 6.7

--- src/main/java/Day10KnotHash.kt
+++ src/main/java/Day10KnotHash.kt
@@ -2,7 +2,7 @@ class Day10KnotHash {
     fun calculateKnotHash(twistLengths: IntArray, elementListLength: Int) : Int {
         val knot = (0 until elementListLength).toList().toIntArray()
         var currentPosition = 0
-        calculateSublist(knot, )
+        // calculateSublist(knot, )
         return 0
     }
 }
