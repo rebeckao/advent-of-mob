ce282604e6c93fc70e14851b1def93e5439d1b5d
FredF78 <fredrikf78@gmail.com>
2020-10-26 19:36:44 +0100

    knot iteration in progress

--- src/main/java/Day10KnotHash.kt
+++ src/main/java/Day10KnotHash.kt
@@ -2,7 +2,15 @@ class Day10KnotHash {
     fun calculateKnotHash(twistLengths: IntArray, elementListLength: Int) : Int {
         val knot = (0 until elementListLength).toList().toIntArray()
         var currentPosition = 0
-        // calculateSublist(knot, )
+        var skipSize = 0
+
+        for ((skipSize, twistLength) in twistLengths.withIndex()) {
+
+        }
+
+
         return 0
     }
+
+
 }
