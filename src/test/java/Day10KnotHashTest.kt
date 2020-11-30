7b62e0e99b539c514e5deabe47b15ade8ff767bb
rebeckao <rebecka.oefverholm@gmail.com>
2020-11-30 18:15:12 +0100

    Parameterize test

--- src/test/java/Day10KnotHashTest.kt
+++ src/test/java/Day10KnotHashTest.kt
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class Day10KnotHashTest {
 
-    @Test
-    fun calculateKnotHash() {
-        val twistLengths = intArrayOf(3, 4, 1, 5)
+    @ParameterizedTest
+    @CsvSource(value = ["3, 4, 1, 5; 12"], delimiter = ';')
+    fun calculateKnotHash(rawLengths : String, expected : Int) {
+        val twistLengths = rawLengths.split(", ").map{it.toInt()}.toIntArray()
         val calculatedKnotHash = Day10KnotHash().calculateKnotHash(twistLengths, 5)
         assertEquals(12, calculatedKnotHash)
     }
