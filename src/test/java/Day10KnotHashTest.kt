fa56e08c02f8e266fc54190afdac5a36e2aaf975
rebeckao <rebecka.oefverholm@gmail.com>
2020-11-30 18:25:31 +0100

    Add more tests

--- src/test/java/Day10KnotHashTest.kt
+++ src/test/java/Day10KnotHashTest.kt
@@ -7,10 +7,23 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class Day10KnotHashTest {
 
     @ParameterizedTest
-    @CsvSource(value = ["3, 4, 1, 5; 12"], delimiter = ';')
-    fun calculateKnotHash(rawLengths : String, expected : Int) {
+    @CsvSource(value = [
+        "3; 5; 2",
+        "3, 4; 5; 12",
+        "3, 4, 1; 5; 12",
+        "3, 4, 1, 5; 5; 12",
+        "1, 4; 5; 0",
+        "1, 2, 2; 5; 8",
+        "1, 3; 3; 0",
+        "3; 3; 2",
+        "3; 3; 2",
+        "3, 3; 3; 0",
+        "3, 2; 5; 2",
+        "3, 2, 3; 3; 0"
+    ], delimiter = ';')
+    fun calculateKnotHash(rawLengths : String, elementListLength : Int, expected : Int) {
         val twistLengths = rawLengths.split(", ").map{it.toInt()}.toIntArray()
-        val calculatedKnotHash = Day10KnotHash().calculateKnotHash(twistLengths, 5)
-        assertEquals(12, calculatedKnotHash)
+        val calculatedKnotHash = Day10KnotHash().calculateKnotHash(twistLengths, elementListLength)
+        assertEquals(expected, calculatedKnotHash)
     }
 }
